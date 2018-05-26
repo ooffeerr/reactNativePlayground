475c776c0d386c2b2e784e963495977ee14cddaf
Ofer Schonberger <ofer.schonberger@wework.com>
2018-05-26 13:01:49 +0300

    text and styles

--- App.js
+++ App.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-class Blink extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {shouldShow: true};
-
-
-    setInterval(() => {
-      this.setState(prevState => {
-          return { shouldShow: !prevState.shouldShow };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.shouldShow ? "lslsls" : "LDLDLDLDL"
-    return (
-      <Text>{display}</Text>
-    );
-  }
+export default class LotsOfStyles extends Component {
+    render() {
+        return (
+            <View>
+                <Text style={styles.red}>just red</Text>
+                <Text style={styles.bigblue}>just bigblue</Text>
+                <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+                <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+                <Text style={[styles.red, styles.green]}>red, then green</Text>
+            </View>
+        );
+    }
 }
 
-export default class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{ alignItems: 'center' }}>
-        <Blink name='Rexxar' />
-        <Blink name='Jaina' />
-        <Blink name='Valeera' />
-      </View>
-    );
-  }
-}
+const styles = StyleSheet.create({
+    bigblue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    red: {
+        color: 'red',
+    },
+    green: {
+      color: 'green',
+        fontSize: 30,
+        textShadowOffset: {
+          width : 20,
+            height: 30,
+        },
+        letterSpacing: 400
+    }
+});
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => LotsOfBlinks);
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
