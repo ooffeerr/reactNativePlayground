e415c126607d94bc89ffb676668d440204fb6ecf
Ofer Schonberger <ofer.schonberger@wework.com>
2018-06-09 18:43:59 +0300

    flex

--- App.js
+++ App.js
@@ -1,39 +1,22 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
-export default class LotsOfStyles extends Component {
+export default class FixedDimensionsBasics extends Component {
     render() {
         return (
-            <View>
-                <Text style={styles.red}>just red</Text>
-                <Text style={styles.bigblue}>just bigblue</Text>
-                <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
-                <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
-                <Text style={[styles.red, styles.green]}>red, then green</Text>
+            <View style = {{flex: 1}}>
+                <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+                    <View style={{flex: 1, backgroundColor: 'red'}}/>
+                    <View style={{flex: 1, backgroundColor: 'yellow'}}/>
+                    <View style={{flex: 1, backgroundColor: 'green'}}/>
+                </View>
+                <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+                <View style={{flex: 3, backgroundColor: 'steelblue'}} />
             </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-    bigblue: {
-        color: 'blue',
-        fontWeight: 'bold',
-        fontSize: 30,
-    },
-    red: {
-        color: 'red',
-    },
-    green: {
-      color: 'green',
-        fontSize: 30,
-        textShadowOffset: {
-          width : 20,
-            height: 30,
-        },
-        letterSpacing: 400
-    }
-});
-
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
+AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
+
