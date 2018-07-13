6bf5709646a96398ab9fca6c0b469fb7f0625cb6
Ofer Schonberger <ofer.schonberger@wework.com>
2018-07-13 17:56:43 +0300

    some pretty colors

--- App.js
+++ App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
 export default class FixedDimensionsBasics extends Component {
+
+
+    colorizedStyle(color) {
+        var standardSizeMap = {width : 50, height: 50};
+        return  {...standardSizeMap, ...{backgroundColor: color}};
+    }
     render() {
+
+
+
         return (
+
             <View style = {{flex: 1}}>
-                <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+                <View style={{flex: 1, backgroundColor: 'powderblue', flexDirection: 'column'}}>
                     <View style={{flex: 1, backgroundColor: 'red'}}/>
-                    <View style={{flex: 1, backgroundColor: 'yellow'}}/>
-                    <View style={{flex: 1, backgroundColor: 'green'}}/>
+                    <View style={this.colorizedStyle('yellow')}/>
+                    <View style={this.colorizedStyle('green')}/>
                 </View>
                 <View style={{flex: 2, backgroundColor: 'skyblue'}} />
                 <View style={{flex: 3, backgroundColor: 'steelblue'}} />
